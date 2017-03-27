341a3cb16664bd4c6c3c4db353471eb1699a4dbb
nicbuitr <n.buitrago29@uniandes.edu.co>
2017-03-27 01:46:13 -0500

    Update

--- server/main.js
+++ server/main.js
@@ -3,6 +3,4 @@ import '../imports/api/teachers.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  process.env.MONGO_URL="mongodb://nicbuitr:admin@ds119750.mlab.com:19750/heroku_42vhqtwt";
-  console.log(process.env);
 });
