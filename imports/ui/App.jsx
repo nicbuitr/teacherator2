3bc00a35e8ff6a48bc2c1213de3a9f3d82ca7953
nicbuitr <n.buitrago29@uniandes.edu.co>
2017-03-28 05:15:57 -0500

    Responsive UX Fixes and Footer Added

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -17,7 +17,7 @@ class App extends Component {
       selectedTeacherId: "",
       queryName:"",
       hideCompleted: false,
-      maxTeachersToList: 4
+      maxTeachersToList: 3
     };
   }
 
@@ -36,11 +36,11 @@ class App extends Component {
     let teacherId  = event.currentTarget.id;
     if (this.state.selectedTeacherId.trim() != ""){
       if (document.getElementById(this.state.selectedTeacherId) != null){
-        document.getElementById(this.state.selectedTeacherId).className = "col-xs-3 teacher-list-element";
+        document.getElementById(this.state.selectedTeacherId).className = "col-xs-4 teacher-list-element";
       }
     }
     this.setState({selectedTeacherId: teacherId});
-    document.getElementById(teacherId).className = "col-xs-3 teacher-list-element-selected";
+    document.getElementById(teacherId).className = "col-xs-4 teacher-list-element-selected";
     //window.location.replace('#teacher-reviews-div');
   }
 
@@ -66,10 +66,10 @@ class App extends Component {
     }
     else{
       return filteredTeachers.slice(0, this.state.maxTeachersToList).map((teacher, index) => (
-        <div key={"teacher_thumbnail_"+teacher._id} id={teacher._id} className="col-xs-3 teacher-list-element" onClick={this.handleClick.bind(this)}>
-            <img key={"teacher_thumbnail_image_"+teacher._id} src={teacher.profile_pic_url} className="teacher-profile-img inline-img-responsive list-img" />
-            <h5 key={"teacher_thumbnail_name_"+teacher._id}>{teacher.name}</h5>
-            <img key={"teacher_thumbnail_stars_"+teacher._id} src={"/"+teacher.avg_review+"_star.png"} className="inline-img-responsive rating-stars-img "/>
+        <div key={"teacher_thumbnail_"+teacher._id} id={teacher._id} className="col-xs-4 teacher-list-element" onClick={this.handleClick.bind(this)}>
+            <img key={"teacher_thumbnail_image_"+teacher._id} src={teacher.profile_pic_url} className="teacher-profile-img inline-img-responsive" />
+            <img key={"teacher_thumbnail_stars_"+teacher._id} src={"/"+teacher.avg_review+"_star.png"} className="inline-img-responsive list-rating-stars-img "/>
+            <h5 key={"teacher_thumbnail_name_"+teacher._id}>{teacher.name}</h5>            
         </div>
       ));
     }
